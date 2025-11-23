f809f1c7f07910b52e34f2befb8ed1d71ac1df87
재선 <ddorri83@naver.com>
2025-11-24 04:16:40 +0900

    Cleand project structure: keep only baseballDiary

--- src/util/get-lg-img.js
+++
@@ -1,9 +0,0 @@
-import lg_emblem from './../assets/lg_emblem.jpg'
-
-
-export function getLgImage(imgId){
-    switch(imgId){
-        case 1 : return lg_emblem;
-        default : return null;
-    }
-}
